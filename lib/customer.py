30a4c22744b1bf1146f4dcbdba236c418afa2a0e
Evan Keya <evan.keya@student.moringaschool.com>
2023-12-11 10:36:00 +0300

    done

--- lib/customer.py
+++ lib/customer.py
@@ -1,19 +1,33 @@
 
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from customers.py import Base, session
+from sqlalchemy import create_engine, Column, Integer, String, Float, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create an engine
+engine = create_engine('sqlite:///customers_table.sqlite')
+
+Base = declarative_base()
+
+class Review(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Float)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+
+    # Define the relationship with Customer
+    customer = relationship('Customer', back_populates='reviews')
+    restaurant = relationship('Restaurant', back_populates='reviews')
 
 class Customer(Base):
     __tablename__ = 'customers'
-    id = Column(Integer, primary_key = True)
+    id = Column(Integer, primary_key=True)
     first_name = Column(String)
     last_name = Column(String)
 
-    # Define the relationship with Review
     reviews = relationship('Review', back_populates='customer')
 
-    # Include the methods for Customer class from the provided code
-    def reviews(self):
+    def get_reviews(self):
         return self.reviews
 
     def restaurants(self):
@@ -36,5 +50,50 @@ class Customer(Base):
             if review.restaurant == restaurant:
                 self.reviews.remove(review)
 
-# Import the Review class if it's in a separate file
-from customers.py import Review
+class Restaurant(Base):
+    __tablename__ = 'restaurants'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+    # Define the relationship with Review
+    reviews = relationship('Review', back_populates='restaurant')
+
+# Create the tables in the database
+Base.metadata.create_all(engine)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Create instances and add them to the session
+
+customer_instance = Customer(first_name='John', last_name='Lee')
+restaurant_instance = Restaurant(name='Nairobi Street Kitchen')
+review_instance = Review(star_rating=4.5, customer=customer_instance, restaurant=restaurant_instance)
+
+customer_instance2 = Customer(first_name='Billy', last_name='Jean')
+restaurant_instance2 = Restaurant(name='KFC')
+review_instance = Review(star_rating=4, customer=customer_instance, restaurant=restaurant_instance)
+
+customer_instance3 = Customer(first_name='Jean', last_name='Pierre')
+restaurant_instance3 = Restaurant(name='Marcados')
+review_instance = Review(star_rating=5, customer=customer_instance, restaurant=restaurant_instance)
+
+# Add instances
+session.add_all([customer_instance, restaurant_instance, review_instance])
+session.add_all([customer_instance2, restaurant_instance2, review_instance])
+session.add_all([customer_instance3, restaurant_instance3, review_instance])
+
+# Commit the changes to the database
+session.commit()
+
+
+customers = session.query(Customer).all()
+for customer in customers:
+    print(f"Customer: {customer.full_name()}, Reviews: {len(customer.get_reviews())}")
+
+restaurants = session.query(Restaurant).all()
+for restaurant in restaurants:
+    print(f"Restaurant: {restaurant.name}, Reviews: {len(restaurant.reviews)}")
+
+
--- lib/restaurant.py
+++ lib/restaurant.py
@@ -1,30 +1,78 @@
+from sqlalchemy import create_engine, Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.ext.declarative import declarative_base
 
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
-from sqlalchemy.orm import relationship
-from your_database_module import Base, session
+# Create an engine
+engine = create_engine('sqlite:///restaurants_table.sqlite')
+
+Base = declarative_base()
+
+Base.metadata.bind = engine
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+class Customer(Base):
+    __tablename__ = 'customer'
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+
+    reviews = relationship('Review', back_populates='customer')
 
 class Restaurant(Base):
-    __tablename__ = 'restaurants'
+    __tablename__ = 'restaurant'
     id = Column(Integer, primary_key=True)
     name = Column(String)
     price = Column(Float)
 
-    # Define the relationship with Review
     reviews = relationship('Review', back_populates='restaurant')
 
-    # Include the methods for Restaurant class from the provided code
-    def reviews(self):
+    def get_reviews(self):
         return self.reviews
 
-    def customers(self):
+    def get_customers(self):
         return [review.customer for review in self.reviews]
 
-    @classmethod
-    def fanciest(cls):
-        return max(session.query(cls).all(), key=lambda restaurant: restaurant.price)
+class Review(Base):
+    __tablename__ = 'reviews'
+    id = Column(Integer, primary_key=True)
+    star_rating = Column(Float)
+    restaurant_id = Column(Integer, ForeignKey('restaurants.id'))
+    customer_id = Column(Integer, ForeignKey('customers.id'))
 
-    def all_reviews(self):
-        return [review.full_review() for review in self.reviews]
+    customer = relationship('Customer', back_populates='reviews')
+    restaurant = relationship('Restaurant', back_populates='reviews')
 
-# Import the Review class if it's in a separate file
-from your_database_module import Review
+# Create instances
+customer_instance = Customer(first_name='John', last_name='Benson')
+restaurant_instance = Restaurant(name='Nairobi Street Kitchen', price=50.0)
+review_instance = Review(star_rating=4.5, customer=customer_instance, restaurant=restaurant_instance)
+
+customer_instance1 = Customer(first_name='Kyle', last_name='Lowry')
+restaurant_instance1 = Restaurant(name='KFC', price=35.0)
+review_instance1 = Review(star_rating= 4.0, customer=customer_instance1, restaurant=restaurant_instance1)
+
+# Add instances to the session
+session.add_all([customer_instance, restaurant_instance, review_instance])
+session.commit()
+
+# Retrieve Restaurant instance
+restaurant_instance = session.query(Restaurant).filter_by(id=1).first()
+
+# Collection of reviews for the restaurant
+reviews_collection = restaurant_instance.get_reviews()
+
+# Collection of customers who reviewed the restaurant
+customers_collection = restaurant_instance.get_customers()
+
+# Printing results
+print("Reviews for the restaurant:")
+for review in reviews_collection:
+    print(f"Review ID: {review.id}, Star Rating: {review.star_rating}")
+
+print("\nCustomers who reviewed the restaurant:")
+for customer in customers_collection:
+    print(f"Customer ID: {customer.id}, Name: {customer.first_name} {customer.last_name}")
